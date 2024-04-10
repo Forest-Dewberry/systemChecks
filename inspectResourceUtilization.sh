afb6b289a781157f37179d02ce2f58627710b856
Forest Dewberry <forest.dewberry@gmail.com>
2024-04-10 22:07:49 +0000

    fixed the ascii art

--- inspectResourceUtilization.sh
+++ inspectResourceUtilization.sh
@@ -14,28 +14,24 @@
 
 # Script to inspect resource utilization
 
-LOGFILE="/var/log/resource_inspection.log"
+# LOGFILE="/var/log/resource_inspection.log"
+LOGFILE="./resource_inspection.log"
 CPU_THRESHOLD=50
 MEM_THRESHOLD=50
 
 # Function to log processes exceeding thresholds
 inspect_resources() {
-    echo " __________                                                  ____ ___   __  .__.__  .__                __  .__               
-" >> $LOGFILE
-    echo " \______   \ ____   __________  __ _________   ____  ____   |    |   \_/  |_|__|  | |__|____________ _/  |_|__| ____   ____  
-" >> $LOGFILE
-    echo " |       _// __ \ /  ___/  _ \|  |  \_  __ \_/ ___\/ __ \  |    |   /\   __\  |  | |  \___   /\__  \\   __\  |/  _ \ /    \ 
-" >> $LOGFILE
-    echo " |    |   \  ___/ \___ (  <_> )  |  /|  | \/\  \__\  ___/  |    |  /  |  | |  |  |_|  |/    /  / __ \|  | |  (  <_> )   |  \
-" >> $LOGFILE
-    echo " |____|_  /\___  >____  >____/|____/ |__|    \___  >___  > |______/   |__| |__|____/__/_____ \(____  /__| |__|\____/|___|  /
-" >> $LOGFILE
-    echo "        \/        \/        \/          \/     \/     \/                         \/    \/                                   \/     \/                    \/ " >> $LOGFILE
+    echo "__________                                                  ____ ___   __  .__.__  .__                __  .__               " >> $LOGFILE
+    echo "\______   \ ____   __________  __ _________   ____  ____   |    |   \_/  |_|__|  | |__|____________ _/  |_|__| ____   ____  " >> $LOGFILE
+    echo " |       _// __ \ /  ___/  _ \|  |  \_  __ \_/ ___\/ __ \  |    |   /\   __\  |  | |  \___   /\__  \\\\   __\  |/  _ \ /    \ " >> $LOGFILE
+    echo " |    |   \  ___/ \___ (  <_> )  |  /|  | \/\  \__\  ___/  |    |  /  |  | |  |  |_|  |/    /  / __ \|  | |  (  <_> )   |  \\" >> $LOGFILE
+    echo " |____|_  /\___  >____  >____/|____/ |__|    \___  >___  > |______/   |__| |__|____/__/_____ \(____  /__| |__|\____/|___|  /" >> $LOGFILE
+    echo "        \/     \/     \/                         \/    \/                                   \/     \/                    \/ " >> $LOGFILE
     echo "Resource utilization report:" >> $LOGFILE
     echo "---------------------------------------------------" >> $LOGFILE
 
     # Print the headers
-    printf "%-10s %-10s %-50s %-6s %-6s\n 
+    # printf "%-10s %-10s %-50s %-6s %-6s\n 
 }
 
 while true; do
