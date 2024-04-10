4d68ffc7c686fe54069ef39eaa6d62d294135cb0
Forest Dewberry <forest.dewberry@gmail.com>
2024-04-10 23:21:31 +0000

    finished inspectResourceUtilization.sh

--- inspectResourceUtilization.sh
+++ inspectResourceUtilization.sh
@@ -20,21 +20,50 @@ CPU_THRESHOLD=50
 MEM_THRESHOLD=50
 
 # Function to log processes exceeding thresholds
-inspect_resources() {
+print_fancy_header() {
     echo "__________                                                  ____ ___   __  .__.__  .__                __  .__               " >> $LOGFILE
     echo "\______   \ ____   __________  __ _________   ____  ____   |    |   \_/  |_|__|  | |__|____________ _/  |_|__| ____   ____  " >> $LOGFILE
     echo " |       _// __ \ /  ___/  _ \|  |  \_  __ \_/ ___\/ __ \  |    |   /\   __\  |  | |  \___   /\__  \\\\   __\  |/  _ \ /    \ " >> $LOGFILE
     echo " |    |   \  ___/ \___ (  <_> )  |  /|  | \/\  \__\  ___/  |    |  /  |  | |  |  |_|  |/    /  / __ \|  | |  (  <_> )   |  \\" >> $LOGFILE
     echo " |____|_  /\___  >____  >____/|____/ |__|    \___  >___  > |______/   |__| |__|____/__/_____ \(____  /__| |__|\____/|___|  /" >> $LOGFILE
     echo "        \/     \/     \/                         \/    \/                                   \/     \/                    \/ " >> $LOGFILE
-    echo "Resource utilization report:" >> $LOGFILE
-    echo "---------------------------------------------------" >> $LOGFILE
+}
+inspect_resources() {
+    date +"%Y-%m-%d %H:%M:%S" > ./datetime.tmp
+    datetime=$( cat ./datetime.tmp )
+    echo "Resource utilization report: " $datetime >> $LOGFILE
+    echo "-----------------------------------" >> $LOGFILE
 
-    # Print the headers
-    # printf "%-10s %-10s %-50s %-6s %-6s\n 
+    # Get a list of all running processes
+    ps aux > ./processes.txt
+
+    # Filter the list of processes by CPU and memory usage
+    # headers
+    head -1 ./processes.txt > ./filtered_processes.txt
+    # Create filtered_processes
+    awk -v cpu_threshold=$CPU_THRESHOLD -v mem_threshold=$MEM_THRESHOLD '$3 > cpu_threshold || $4 > mem_threshold {print $0}' ./processes.txt >> ./filtered_processes.txt
+
+    # If processes over either limit are found then echo a warning message to stdout
+    linecount=$( cat -n ./filtered_processes.txt | tail -1 | awk '{print $1}' )
+    adjusted_count=$((linecount - 1))
+    if [ $adjusted_count -gt 0 ]; then
+        echo "warning: Processes with CPU or MEM above limit were identified! Look in $LOGFILE"
+    fi
+
+    # Log the filtered list of processes
+    echo "Processes above thresholds follow. CPU_THRESHOLD is " $CPU_THRESHOLD " and MEM_THRESHOLD is " $MEM_THRESHOLD >> $LOGFILE
+    cat ./filtered_processes.txt >> $LOGFILE
+
+    # Remove the temporary files
+    # rm ./processes.txt ./filtered_processes.txt
+
+    echo "-----------------------------------" >> $LOGFILE
 }
 
-while true; do
-    inspect_resources
-    sleep 60 # sleeps for 60 sec
-done
+print_fancy_header
+inspect_resources
+# Comment the above and uncomment the below to Check every minute (to avoid having to use cron. Cron is what I'd recommend, though.)
+# while true; do
+#     inspect_resources
+#     sleep 60  # Sleep for 60 seconds
+# done
